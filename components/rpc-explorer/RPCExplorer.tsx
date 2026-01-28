3d49674c84ac872af16ab65fa3d313270506ee8b
0xlean <0xleanlp@gmail.com>
2026-01-28 17:47:16 -0300

    Fix: Point RPC Explorer to correct API URL

--- components/rpc-explorer/RPCExplorer.tsx
+++ components/rpc-explorer/RPCExplorer.tsx
@@ -108,7 +108,7 @@ export default function RPCExplorer() {
         setInput('');
 
         try {
-            const res = await fetch('http://localhost:4000/api/rpc-playground', {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000'}/api/rpc-playground`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ method, params })
